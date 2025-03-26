9d375ae57728a852eed4c327f24bc39e700075f5
vcpersike <vcpersike@gmail.com>
2025-03-26 02:59:43 -0300

    Adicionar arquivos de projeto.

---
+++ AppFinancas.Shared/Services/IFormFactor.cs
@@ -0,0 +1,8 @@
+namespace AppFinancas.Shared.Services
+{
+    public interface IFormFactor
+    {
+        public string GetFormFactor();
+        public string GetPlatform();
+    }
+}
---
+++ AppFinancas.Web/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using AppFinancas.Shared.Services;
+
+namespace AppFinancas.Web.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return "Web";
+        }
+
+        public string GetPlatform()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
